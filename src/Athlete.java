6a01055e6a49b743615653624265ca817f208ebe
Isaac Donkoh-Halm <ikdonkohhalm@wpi.edu>
2016-10-26 21:09:34 -0400

    First Commit

---
+++ src/Athlete.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Isaac Donkoh-Halm on 10/26/2016.
+ */
+public class Athlete {
+    int CyclingResult;
+    double BiathlonResult;
+    Athlete(int CyclingResult, double BiathlonResult){
+        this.CyclingResult=CyclingResult;
+        this.BiathlonResult=BiathlonResult;
+    }
+
+}
