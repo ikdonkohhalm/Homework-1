b27a894e7c76e45a091a707c43b00245169995de
Isaac Donkoh-Halm <ikdonkohhalm@wpi.edu>
2016-10-26 21:23:00 -0400

    Up to question 3 done.
    How to do rounds?

--- src/Athlete.java
+++ src/Athlete.java
@@ -2,11 +2,23 @@
  * Created by Isaac Donkoh-Halm on 10/26/2016.
  */
 public class Athlete {
-    int CyclingResult;
-    double BiathlonResult;
-    Athlete(int CyclingResult, double BiathlonResult){
+    CyclingResult;
+    BiathlonResult;
+    Athlete(CyclingResult, BiathlonResult){
         this.CyclingResult=CyclingResult;
         this.BiathlonResult=BiathlonResult;
     }
-
+class CyclingResult {
+    double time;
+    int position;
+    CyclingResult(double time, int position){
+        this.time=time;
+        this.position=position;
+    }
+class BiathlonResult {
+    int targetHit;
+    double trackRunTime;
+    
+}
+}
 }
