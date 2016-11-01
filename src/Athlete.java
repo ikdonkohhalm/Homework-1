3e2db972240414762cfcbd13329f3753eecabd1d
ikdonkohhalm <ikdonkohhalm@wpi.edu>
2016-11-01 11:14:10 -0400

    Problems 1 and 2 (tutor)

--- src/Athlete.java
+++ src/Athlete.java
@@ -2,11 +2,21 @@
  * Created by Isaac Donkoh-Halm on 10/26/2016.
  */
 public class Athlete {
-    int CyclingResult;
     double BiathlonResult;
-    Athlete(int CyclingResult, double BiathlonResult){
-        this.CyclingResult=CyclingResult;
+    CyclingResult cr;
+    public Athlete(double BiathlonResult, CyclingResult cr){
         this.BiathlonResult=BiathlonResult;
+        this.cr=cr;
     }
 
 }
+
+public class CyclingResult
+    double time;
+    int position;
+    public CyclingResult (double time, int position) {
+        this.time=time;
+        this.position=position
+    }
+}
+
