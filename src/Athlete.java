fdd4af5deea4ad23d82d982caf7e5b637744d469
ikdonkohhalm <ikdonkohhalm@wpi.edu>
2016-11-01 11:23:30 -0400

    Question 3 (Tutor)

--- src/Athlete.java
+++ src/Athlete.java
@@ -2,16 +2,16 @@
  * Created by Isaac Donkoh-Halm on 10/26/2016.
  */
 public class Athlete {
-    double BiathlonResult;
+    BiathlonResult br;
     CyclingResult cr;
-    public Athlete(double BiathlonResult, CyclingResult cr){
-        this.BiathlonResult=BiathlonResult;
+    public Athlete(BiathlonResult br, CyclingResult cr){
+        this.br=br;
         this.cr=cr;
     }
 
 }
 
-public class CyclingResult
+public class CyclingResult {
     double time;
     int position;
     public CyclingResult (double time, int position) {
@@ -20,3 +20,25 @@ public class CyclingResult
     }
 }
 
+
+public class BiathlonResult {
+    BiathlonRound round1;
+    BiathlonRound round2;
+    BiathlonRound round3;
+    public BiathlonResult (BiathlonRound round1, BiathlonRound round2, BiathlonRound round3) {
+        this.round1=round1;
+        this.round2=round2;
+        this.round3=round3;
+    }
+    
+}
+
+
+public class BiathlonRound {
+    int hitTargets;
+    double runTime;
+    public BiathlonRound (int hitTargets, double runTime){
+        this.hitTargets=hitTargets;
+        this.runTime=runTime
+    }
+}
